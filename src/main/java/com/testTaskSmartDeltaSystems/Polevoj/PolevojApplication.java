624b5a320e8a6dd7406fb6caba7825943f9aa4cc
artem <artempolevoj@yandex.ru>
2024-11-02 21:08:29 +0300

    create project

---
+++ src/main/java/com/testTaskSmartDeltaSystems/Polevoj/PolevojApplication.java
@@ -0,0 +1,13 @@
+package com.testTaskSmartDeltaSystems.Polevoj;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PolevojApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PolevojApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/testTaskSmartDeltaSystems/Polevoj/PolevojApplicationTests.java
@@ -0,0 +1,13 @@
+package com.testTaskSmartDeltaSystems.Polevoj;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PolevojApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
