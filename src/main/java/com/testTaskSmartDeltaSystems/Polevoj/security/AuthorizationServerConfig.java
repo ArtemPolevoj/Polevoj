c08d1750a98cd734bc8530c2e05d3b00ccbd5300
artem <artempolevoj@yandex.ru>
2024-11-04 06:50:43 +0300

    deleted unnecessary

--- src/main/java/com/testTaskSmartDeltaSystems/Polevoj/security/AuthorizationServerConfig.java
+++ src/main/java/com/testTaskSmartDeltaSystems/Polevoj/security/AuthorizationServerConfig.java
@@ -1,14 +1,11 @@
 package com.testTaskSmartDeltaSystems.Polevoj.security;
 
-
-
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 
-import java.util.Collections;
 @Configuration
 public class AuthorizationServerConfig {
     @Bean
@@ -16,10 +13,7 @@ public class AuthorizationServerConfig {
         httpSecurity
 
                 .authorizeHttpRequests((authorize) ->
-                        authorize.requestMatchers("students/**").authenticated() //permitAll()
-//                                .requestMatchers("persons/**", "admin/**", "v3/api-docs/**", "swagger-ui/**").hasAnyAuthority("ADMIN")
-//                                .requestMatchers("/students/**").hasAnyAuthority("USER", "ADMIN")
- //                               .requestMatchers("auth/**", "any/**", "/", "index/**").authenticated()
+                        authorize.requestMatchers("students/**").authenticated()
                                 .anyRequest().denyAll()
                 )
                 .formLogin(Customizer.withDefaults()
